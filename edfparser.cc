e426da83ba40b690a0ff05ccca8013893a1eed14
rjp <rjpartington@googlemail.com>
2010-08-25 13:46:24 +0100

    - initial sketchy node.js extension for edf parsing

---
+++ edfparser.cc
@@ -0,0 +1,72 @@
+/* This code is PUBLIC DOMAIN, and is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND. See the accompanying 
+ * LICENSE file.
+ */
+
+#include <v8.h>
+#include <node.h>
+
+using namespace node;
+using namespace v8;
+
+class EDFParser: ObjectWrap
+{
+private:
+  int m_count;
+public:
+
+  static Persistent<FunctionTemplate> s_ct;
+  static void Init(Handle<Object> target)
+  {
+    HandleScope scope;
+
+    Local<FunctionTemplate> t = FunctionTemplate::New(New);
+
+    s_ct = Persistent<FunctionTemplate>::New(t);
+    s_ct->InstanceTemplate()->SetInternalFieldCount(1);
+    s_ct->SetClassName(String::NewSymbol("EDFParser"));
+
+    NODE_SET_PROTOTYPE_METHOD(s_ct, "parse", Parse);
+
+    target->Set(String::NewSymbol("EDFParser"),
+                s_ct->GetFunction());
+  }
+
+  EDFParser() :
+    m_count(0)
+  {
+  }
+
+  ~EDFParser()
+  {
+  }
+
+  static Handle<Value> New(const Arguments& args)
+  {
+    HandleScope scope;
+    EDFParser* hw = new EDFParser();
+    hw->Wrap(args.This());
+    return args.This();
+  }
+
+  static Handle<Value> Parse(const Arguments& args)
+  {
+    HandleScope scope;
+    EDFParser* hw = ObjectWrap::Unwrap<EDFParser>(args.This());
+    hw->m_count++;
+    Local<String> result = String::New("PARSE MY EDF");
+    return scope.Close(result);
+  }
+
+};
+
+Persistent<FunctionTemplate> EDFParser::s_ct;
+
+extern "C" {
+  static void init (Handle<Object> target)
+  {
+    EDFParser::Init(target);
+  }
+
+  NODE_MODULE(edfparser, init);
+}
