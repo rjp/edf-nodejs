08f4b8fc29c67879c783aa019d487b168ddcfea4
rjp <zimpenfish@gmail.com>
2010-08-27 14:45:24 +0100

    - update test.js to reflect the new parsing return

--- test.js
+++ test.js
@@ -2,8 +2,17 @@ assert = require('assert');
 sys = require('sys');
 edf = require('edfparser');
 parser = new edf.EDFParser();
-j = parser.parse("<edf=\"on\"><test=42/></>");
+
+
+//j = parser.parse("<edf=\"on\"><test=42/></>");
+j = parser.parse("<edf=\"on\"><test=42/></><reply=\"fish\"><age=44/></>");
 e = eval('('+j+')');
-assert.equal('edf', e.tag);
-assert.equal('on', e.value);
-assert.equal(1, e.children.length);
+
+assert.equal(2, e.parsed);
+
+assert.equal('edf', e.trees[0].tag);
+assert.equal('on', e.trees[0].value);
+assert.equal(1, e.trees[0].children.length);
+
+assert.equal('reply', e.trees[1].tag);
+assert.equal('fish', e.trees[1].value);
