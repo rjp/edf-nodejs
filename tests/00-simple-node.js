cbd8b675b21c0c2fb0a527e05b13c47410dc6014
rjp <rjpartington@googlemail.com>
2010-09-11 12:21:28 +0100

    - test suite, very simplistic to start with

---
+++ tests/00-simple-node.js
@@ -0,0 +1,20 @@
+assert = require('assert');
+edf = require('edfparser');
+parser = new edf.EDFParser();
+
+j = parser.parse('<edf="on"/>');
+e = eval('('+j+')');
+
+exports['test parse count'] = function(test){
+    test.expect(1);
+    test.equals(e.parsed, 1, 'number of parsed nodes');
+    test.done();
+}
+
+exports['test parsed node'] = function(test){
+    test.expect(3);
+    test.equals(e.trees[0].tag, 'edf', 'edf node');
+    test.equals(e.trees[0].value, 'on', 'edf node');
+    test.equals(e.trees[0].children, undefined, 'edf node: no children');
+    test.done();
+}
